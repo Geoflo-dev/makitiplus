265f7da284f9aeccdccc12387026b3e99f17d123
Geoflo-dev <80064999+Geoflo-dev@users.noreply.github.com>
2021-03-05 18:00:43 +0000

    Ajout des assets svg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
 <template>
+  <div class="app">
+    <!-- le menu de navigation-->
+    <div class="container is-fullhd">
+        <div class="navbar-top">
+        <!-- plutard -->
+        </div>
+        <!-- Menu de naviagation -->
+        <div class="nav-section">
+            <div class="columns">
+                  <div class="column is-2">
+                    First column
+                  </div>
+                  <div class="column is-1">
+                    Second column
+                  </div>
+                  <div class="column is-5">
+                    Third column
+                  </div>
+                  <div class="column is-2">
+                    Fourth column
+                  </div>
+                  <div class="column is-1">
+                   fith column
+                  </div>
+                  <div class="column is-1">
+                     sith column
+                  </div>
+                  
+                </div>
+        </div>
+    </div>
+
+  </div>
   
 </template>
+<script>
+export default {
+  
+}
+</script>
 
 <style lang="scss">
+  .navbar-top{
+    width: 1440px;
+    height: 34px;
+    left: 0px;
+    top: 0px;
+   /* Blue Gradient btn */
+    background: linear-gradient(270deg, #067AC2 0%, #209EDB 100%), #C4C4C4;
+   }
+   .nav-section{
+    width: 1440px;
+    height: 89px;
+    left: 0px;
+    top: 34px;
 
+    background: #FFFFFF;
+   }
 </style>
